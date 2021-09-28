6cb04a402c700937a11850c4ba56d395e528cf39
trinhvu-realdude <vudangtrinh29102000@gmail.com>
2021-09-28 07:42:21 +0700

    update button close, iframe stop

--- script.js
+++ script.js
@@ -19,26 +19,56 @@ const onItemToPopUp4 = () => {
     document.getElementById('pop-up-04').style.display = 'block';
 }
 
+let pop_up_content = document.getElementById('pop-up-content');
+
+let iframes = document.getElementsByTagName('iframe');
+
 const buttonClose1 = () => {
     document.getElementById('pop-up-01').style.display = 'none';
+    if (iframes != null) {
+        for (let i = 0; i < iframes.length; i++) {
+            iframes[i].src = iframes[i].src;
+        }
+    }
 }
 
 const buttonClose2 = () => {
     document.getElementById('pop-up-02').style.display = 'none';
+    if (iframes != null) {
+        for (let i = 0; i < iframes.length; i++) {
+            iframes[i].src = iframes[i].src;
+        }
+    }
 }
 
 const buttonClose3 = () => {
     document.getElementById('pop-up-03').style.display = 'none';
+    if (iframes != null) {
+        for (let i = 0; i < iframes.length; i++) {
+            iframes[i].src = iframes[i].src;
+        }
+    }
 }
 
 const buttonClose4 = () => {
     document.getElementById('pop-up-04').style.display = 'none';
+    if (iframes != null) {
+        for (let i = 0; i < iframes.length; i++) {
+            iframes[i].src = iframes[i].src;
+        }
+    }
 }
+
 window.onclick = (event) => {
     if(event.target == new_window_1 || event.target == new_window_2 || event.target == new_window_3 || event.target == new_window_4) {
         new_window_1.style.display = 'none';
         new_window_2.style.display = 'none';
         new_window_3.style.display = 'none';
         new_window_4.style.display = 'none';
+        if (iframes != null) {
+            for (let i = 0; i < iframes.length; i++) {
+                iframes[i].src = iframes[i].src;
+            }
+        }
     }
 }
