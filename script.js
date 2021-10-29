1c4744651b9dde9f51105467617a81c0e20baecf
trinhvu-realdude <vudangtrinh29102000@gmail.com>
2021-10-30 00:38:15 +0700

    update windowPopUp

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-let new_window_1 = document.getElementById('pop-up-01');
-let new_window_2 = document.getElementById('pop-up-02');
-let new_window_3 = document.getElementById('pop-up-03');
-let new_window_4 = document.getElementById('pop-up-04');
-
-const onItemToPopUp1 = () => {
-    document.getElementById('pop-up-01').style.display = 'block';
-}
-
-const onItemToPopUp2 = () => {
-    document.getElementById('pop-up-02').style.display = 'block';
-}
-
-const onItemToPopUp3 = () => {
-    document.getElementById('pop-up-03').style.display = 'block';
-}
-
-const onItemToPopUp4 = () => {
-    document.getElementById('pop-up-04').style.display = 'block';
-}
-
-let pop_up_content = document.getElementById('pop-up-content');
-
 let iframes = document.getElementsByTagName('iframe');
 
 const buttonClose1 = () => {
@@ -55,20 +32,6 @@ const buttonClose4 = () => {
     if (iframes != null) {
         for (let i = 0; i < iframes.length; i++) {
             iframes[i].src = iframes[i].src;
-        }
-    }
-}
-
-window.onclick = (event) => {
-    if (event.target == new_window_1 || event.target == new_window_2 || event.target == new_window_3 || event.target == new_window_4) {
-        new_window_1.style.display = 'none';
-        new_window_2.style.display = 'none';
-        new_window_3.style.display = 'none';
-        new_window_4.style.display = 'none';
-        if (iframes != null) {
-            for (let i = 0; i < iframes.length; i++) {
-                iframes[i].src = iframes[i].src;
-            }
         }
     }
 }
@@ -121,5 +84,21 @@ const app = new Vue({
                 max_price: 25000
             },
         ]
+    },
+    methods: {
+        windowPopUp: function (id) {
+            for (let i = 0; i < this.list.length; i++) {
+                if (id === this.list[i].id) {
+                    document.getElementById(`pop-up-0${id}`).style.display = 'block';
+                }
+            }
+        },
+        buttonClose: function (id) {
+            for (let i = 0; i < this.list.length; i++) {
+                if (id === this.list[i].id) {
+                    console.log(id);
+                }
+            }
+        }
     }
 });
